ad2a75521c7b92ed8c7aefbcfaac2ffd4d4f1602
Tiago David Furtado <tiagofur@gmail.com>
2020-06-28 22:39:12 -0500

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// RUTAS
+import { APP_ROUTES } from './app.routes';
+
+import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    NavbarComponent,
+    SidebarComponent,
+    BreadcrumbsComponent,
+    PagesComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
